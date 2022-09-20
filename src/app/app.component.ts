862725ba88739b3388f5b5aba23f56aa85dd82c7
Dmitry Komkov <dkomkov@gmail.com>
2022-09-21 00:02:40 +0200

    Second step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +10,50 @@ export class AppComponent implements OnInit {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
   image: HTMLImageElement;
+  isDragging: boolean = false;
+  dragStart: { x: number, y: number } = { x: 0, y: 0 };
+  cameraOffset: { x: number, y: number } = { x: 0, y: 0};
 
   ngOnInit() {
     this.canvas = this.canvasRef.nativeElement;
     this.ctx = this.canvas.getContext('2d')!;
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
 
     this.image = new Image();
     this.image.src = 'assets/8/image-4-4.webp';
-    this.image.onload = () => {
-      this.ctx.drawImage(this.image, 0, 0);
+    this.draw();
+  }
+
+  draw() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    if (this.image.complete) {
+      this.ctx.drawImage(this.image, this.cameraOffset.x, this.cameraOffset.y);
+    } else {
+      this.image.onload = () => {
+        this.ctx.drawImage(this.image, this.cameraOffset.x, this.cameraOffset.y);
+      }
+    }
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  onPointerDown(event: MouseEvent) {
+    this.isDragging = true;
+    this.dragStart.x = event.clientX - this.cameraOffset.x;
+    this.dragStart.y = event.clientY - this.cameraOffset.y;
+  }
+
+  @HostListener('document:mouseup')
+  onPointerUp() {
+    this.isDragging = false;
+  }
+
+  @HostListener('document:mousemove',  ['$event'])
+  onPointerMove(event: MouseEvent) {
+    if (this.isDragging) {
+      this.cameraOffset.x = event.clientX - this.dragStart.x;
+      this.cameraOffset.y = event.clientY - this.dragStart.y;
+      this.draw();
     }
   }
 }
