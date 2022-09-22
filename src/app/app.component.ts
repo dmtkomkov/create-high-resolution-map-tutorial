43ea457d3be1b30869a4aa1c25d7181b3d8369a9
Dmitry Komkov <dkomkov@gmail.com>
2022-09-22 22:30:27 +0200

    Fifth step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 const ZOOM_FACTOR = 1.2;
-const MAX_ZOOM = 5;
-const MIN_ZOOM = -5;
+const MAX_ZOOM = 10;
+const MIN_ZOOM = -10;
 const ORIGINAL_TILE_SIZE = 2048;
+
+const LAYER_SIZE = 2;
 
 @Component({
   selector: 'app-root',
@@ -14,39 +16,52 @@ export class AppComponent implements OnInit {
   @ViewChild('canvas', { static: true }) canvasRef: ElementRef<HTMLCanvasElement>;
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
-  image: HTMLImageElement;
+  layer: HTMLImageElement[][] = [];
   isDragging: boolean = false;
   dragStart: { x: number, y: number } = { x: 0, y: 0 };
   cameraOffset: { x: number, y: number } = { x: 0, y: 0};
-  zoomLevel = -4;
+  zoomLevel = -5;
   cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel);
 
   ngOnInit() {
     this.canvas = this.canvasRef.nativeElement;
     this.ctx = this.canvas.getContext('2d')!;
 
-    this.image = new Image();
-    this.image.src = 'assets/8/image-4-4.webp';
+    for (let i=0; i < LAYER_SIZE; i++) {
+      this.layer[i] = [];
+      for (let j=0; j < LAYER_SIZE; j++) {
+        this.layer[i][j] = new Image();
+      }
+    }
+
+    // this.image.src = 'assets/8/image-4-4.webp';
     this.draw();
   }
 
   draw() {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
+    this.ctx.translate(this.cameraOffset.x, this.cameraOffset.y);
 
     const tileSize: number = ORIGINAL_TILE_SIZE * this.cameraZoom;
-
-    if (this.image.complete) {
-      this.drawImage(this.cameraOffset.x, this.cameraOffset.y, tileSize);
-    } else {
-      this.image.onload = () => {
-        this.drawImage(this.cameraOffset.x, this.cameraOffset.y, tileSize);
+    for (let tileX = 0; tileX < LAYER_SIZE; tileX++) {
+      for (let tileY = 0; tileY < LAYER_SIZE; tileY++) {
+        const image = this.layer[tileX][tileY];
+        if (!image.src) image.src = `assets/${LAYER_SIZE}/image-${tileX}-${tileY}.webp`
+        this.drawImageTile(this.layer[tileX][tileY], tileX, tileY, tileSize);
       }
     }
   }
 
-  drawImage(x: number, y: number, size: number) {
-    this.ctx.drawImage(this.image, x, y, size, size);
+
+  drawImageTile(image: HTMLImageElement, tileX: number, tileY: number, tileSize: number) {
+    if (image.complete) {
+      this.ctx.drawImage(image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
+    } else {
+      image.onload = () => {
+        this.ctx.drawImage(image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
+      }
+    }
   }
 
   @HostListener('document:mousedown', ['$event'])
